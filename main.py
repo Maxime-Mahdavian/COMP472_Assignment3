9e14141abbb0b9dd562ec8b1f9c9e8971055b7b6
Maxime-Mahdavian <Mordicus2018>
2021-11-20 15:46:20 -0500

    Things work now,  must do other models, gotta wait

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from gensim import corpora, models, similarities, downloader
 import pandas as pd
 import json
 
-def main():
 
+def main():
     doc = pd.read_csv('synonyms.csv')
     name_model = 'word2vec-google-news-300'
-    name_model = 'glove-twitter-200'
+    # name_model = 'glove-twitter-200'
     model = downloader.load(name_model)
     correct_guesses = 0
     answer_without_guessing = 0
+    vocab = []
+    output_file = open(name_model + '-details.csv', 'w')
 
-    output_file = open(name_model+'-details.csv', 'w')
+    for index, word in enumerate(model.index_to_key):
+        vocab.append(word)
 
     for i in range(80):
         row = doc.loc[i, :]
@@ -19,29 +22,34 @@ def main():
         answer_word = row.loc["answer"]
         try:
             similars = model.most_similar(question_word)
+            answer_without_guessing += 1
+            guess_word = similars[0][0]
+
+            choices = row["0":]
+            count = 0
+            for j in range(len(choices)):
+                if choices[j] in vocab == False:
+                    count += 1
+
+            if count == 4:
+                ans = "guess"
+            elif answer_word == guess_word:
+                ans = "correct"
+                correct_guesses += 1
+            else:
+                ans = "wrong"
+            output = str(question_word) + "," + str(answer_word) + "," + str(guess_word) + "," + str(ans) + "\n"
         except KeyError:
-            continue
+            output = str(question_word) + "," + str(answer_word) + "," + "NO ANSWER,guess\n"
 
-        answer_without_guessing += 1
-        guess_word = similars[0][0]
-
-        if answer_word == guess_word:
-            ans = "correct"
-            correct_guesses += 1
-        else:
-            ans = "wrong"
-        output = str(question_word) + "," + str(answer_word) + "," + str(guess_word) + "," + str(ans) + "\n"
         output_file.write(output)
 
     analysis_file = open("analysis.csv", "a")
 
     analysis_output = name_model + ',' + str(len(model.index_to_key)) + ',' + str(correct_guesses) + ',' \
-        + str(answer_without_guessing) + ',' + str(correct_guesses/answer_without_guessing) + "\n"
+                      + str(answer_without_guessing) + ',' + str(correct_guesses / answer_without_guessing) + "\n"
 
     analysis_file.write(analysis_output)
-
-
-
 
 
 if __name__ == "__main__":
